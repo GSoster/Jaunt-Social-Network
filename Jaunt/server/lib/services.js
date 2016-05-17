9dffda8d84efaaaa2d4a341aaaaf8d7d6f340b3b
GSoster <guilhermesosters@gmail.com>
2016-05-16 22:50:38 -0300

    updating directories and style guide descriptions

--- Jaunt/server/lib/services.js
+++
@@ -1,7 +0,0 @@
-/*this file is used to configure and manage third party services (facebook) like to  make sure that there are not duplicated accounts*/
-ServiceConfiguration.configurations.remove({});
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: 'id',
-    secret: 'Thesecret'
-});
