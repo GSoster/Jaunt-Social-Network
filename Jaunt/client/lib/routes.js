25fc36f28fc5a64df656cf427e5038cde5291726
GSoster <guilhermesosters@gmail.com>
2016-05-22 18:41:17 -0300

    added new mongo Collection (Friendships), starting the logic necessary to add/remove friends

--- Jaunt/client/lib/routes.js
+++ Jaunt/client/lib/routes.js
@@ -8,11 +8,30 @@ Router.map(function(){
       template: "user",
       layoutTemplate : "layout",
       data : function(){
-        var _id = this.params._id;
+        var _id = this.params._id;//other user id (the one which the profile our currentUser is visiting )
+        var isFollowing = Friendships.isFollowing(_id);
+        Session.set("currentUserId", _id);
+        Session.set("isFollowing", isFollowing);
         return {
           user : Meteor.users.findOne({_id : _id}),
           posts : Posts.list(_id)
         }
+      }
+    });
+    this.route("follow", {
+      path: "/user/:_id/follow",
+      action : function(){
+      var _id = this.params._id;
+      Friendships.follow(_id);
+      this.redirect("/user/"+_id);
+      }
+    });
+    this.route("unfollow", {
+      path: "/user/:_id/unfollow",
+      action : function(){
+        var _id = this.params._id;
+        Friendships.unfollow(_id);
+        this.redirect("/user/"+_id);
       }
     });
  });
---
+++ Jaunt/models/friendships.js
@@ -0,0 +1,29 @@
+Friendships = new Meteor.Collection("friendships");
+
+/**
+* creates a new relation between two users.
+*/
+Friendships.follow = function(friendId){
+  this.insert({userId : this.userId,
+    friendId : friendId
+  });
+};
+
+/**
+* removes a relationship between two users.
+*/
+Friendships.unfollow = function(friendId){
+  this.remove({
+    userId : this.userId,
+      friendId : friendId
+  });
+};
+
+/**
+* Verifies if two users (currentUser and possible friend) have a relationship.
+returns the relationship if it exists or undefined otherwise.
+*/
+Friendships.isFollowing = function(friendId){
+  return this.findOne({userId : this.userId,
+    friendId : friendId});
+};
