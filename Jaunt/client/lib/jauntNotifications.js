158d063f2dcf307e02297cf0b160ed94e00cc2a0
GSoster <guilhermesosters@gmail.com>
2016-08-23 23:50:21 -0300

    added notification for new user's post

---
+++ Jaunt/client/lib/jauntNotifications.js
@@ -0,0 +1,14 @@
+//Global object
+//can't use var here because Meteor declares a variable with var only to the current file.
+jauntNotifications = {};
+//docs here: http://s-alert.meteorapp.com/#
+
+//new post published notification
+jauntNotifications.postPublishedNotification = function(){
+  sAlert.success('Post published!!', {position: 'bottom-right', timeout: 'none'});
+};
+
+
+jauntNotifications.friendAdded = function(followingsName){
+  sAlert.info('You are now following' + followingsName + ' !!', {position: 'bottom-right', timeout: 'none'});
+};
