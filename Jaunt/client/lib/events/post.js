c867014bee63b665683b5432c74109789389cc9e
GSoster <guilhermesosters@gmail.com>
2016-10-13 22:22:52 -0300

    refactoring: removing unnecessary folders and assets, logs and debugs

--- Jaunt/client/lib/events/post.js
+++ Jaunt/client/lib/events/post.js
@@ -17,14 +17,12 @@ Template.post.events({
 });
 
 Template.post.onRendered(function() {
-    console.log("renderizou!");
     tinymce.EditorManager.editors = []; //we need to remove the old instances.
     tinymce.init({
       selector: '#newPostTextArea',
-      height : 300,
+      /*height : 200,*/
       skin_url: '/packages/teamon_tinymce/skins/lightgray',
       menubar: false,
       statusbar: false
     });
-    console.log("carregou o skin");
 });
