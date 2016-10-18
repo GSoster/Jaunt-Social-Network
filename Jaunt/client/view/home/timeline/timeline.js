20c77777b41b3a9367d71bb21ba2d72ba3a17c82
GSoster <guilhermesosters@gmail.com>
2016-10-17 23:55:34 -0200

    comments now display emoticons

--- Jaunt/client/view/home/timeline/timeline.js
+++ Jaunt/client/view/home/timeline/timeline.js
@@ -4,7 +4,17 @@ Template.timeline.events({
   "click #js-commentPost": function(event, template){
      event.preventDefault();
      var postId = this._id;
-     Session.set('postBeingCommented', postId);     
+     Session.set('postBeingCommented', postId);
      $('#commentModal').modal('show');
   }
+});
+
+
+Template.timeline.helpers({
+  isEmoticonEnabled : function(){
+    var points = Points.pointsCountFromUser(Meteor.userId());
+    console.log(points);
+    return true;
+  }
+
 });
