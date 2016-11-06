b9841fb52b5aecb85bdd830981d7adb8b2e6d9e2
GSoster <guilhermesosters@gmail.com>
2016-11-06 19:24:15 -0200

    added gridTemaplte option

---
+++ Jaunt/client/view/home/timeline/gridTimeline.js
@@ -0,0 +1,37 @@
+Template.gridTimeline.events({
+  /*it adds a specific class to the post being commented
+    so the modal can find it and relate to it   */
+  "click #js-commentPost": function(event, template){
+     event.preventDefault();
+     var postId = this._id;
+     Session.set('postBeingCommented', postId);
+     $('#commentModal').modal('show');
+  }
+});
+
+
+Template.gridTimeline.helpers({
+  /*the player needs at least 35 points to enable emoticons on his comments*/
+  isEmoticonEnabled : function(){
+    var points =  Points.listPointsFromUser(Meteor.userId());
+    var totalPoints = 0;
+    points.map(function(x){totalPoints += x.points;});
+    if (totalPoints > 35){
+      return true;
+    }
+    return false;
+  }
+
+});
+
+Template.gridTimeline.onRendered(function(){
+  $(function (){
+				$('.grid').isotope({
+			  itemSelector: '.grid-item',
+			  percentPosition: true,
+			  masonry: {
+				columnWidth: '.grid-sizer'
+			  }
+			});
+			});
+});
