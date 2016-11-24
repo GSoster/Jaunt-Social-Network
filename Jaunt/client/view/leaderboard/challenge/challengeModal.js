ef927402ee23b1b37bf875950b886fd19ef33b19
GSoster <guilhermesosters@gmail.com>
2016-11-24 21:23:20 -0200

    modeled the challenge document to be stored on database

--- Jaunt/client/view/leaderboard/challenge/challengeModal.js
+++ Jaunt/client/view/leaderboard/challenge/challengeModal.js
@@ -10,24 +10,22 @@ Template.challengeModal.helpers({
 Template.challengeModal.events({
   "click #js-challengeModal-save": function(event, template){
     event.preventDefault();
-
-      $('#challengeModal').modal('hide');//dimiss modal
-
     var friendId = Session.get('friendBeingChallenged');
-     /*var comment = {
-      userId: Meteor.userId(),
-      userName: Meteor.user().profile.firstName,
-      date: new Date(),
-      text: commentText,
-      postId: postId
+    console.log("Challenge called for the friend: " + friendId);
+    var challenge = {
+      dateChallenged : new Date(),
+      challengedUser: friendId,
+      challengedBy: Meteor.userId(),
+      pointsSpentToChallenge: 5,
+      pointsToReceive: 2,
+      completed: false,
+      completedOn: null,
     };
-    Meteor.call('commentOnPost', comment, postId);
-    Meteor.call("increasePontuation", jauntAchievementsRules.CommentPointsIncrease, 'comment', Meteor.userId());
-    $('#userComment').val('');//cleans the modal textbox
-    $('#commentModal').modal('hide');//dimiss modal
-    jauntNotifications.defaultCommentPublised();//calls notification
-    jauntAchievementsRules.checkAndUnlockCommentAchievementByCondition(Meteor.userId());
-    return false;*/
+    //Meteor.call('commentOnPost', comment, postId);
+    //jauntNotifications.defaultCommentPublised();//calls notification
+    console.log(challenge);
+    $('#challengeModal').modal('hide');//dimiss modal
+    return;    
   },
 
 });
