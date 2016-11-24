7e553a461cbe15038360ab8d2bd9a6c89b8310f7
GSoster <guilhermesosters@gmail.com>
2016-11-24 18:34:58 -0200

    fixed the display list of friends in the search friends template

--- Jaunt/client/view/header/searchFriend/searchFriend.js
+++ Jaunt/client/view/header/searchFriend/searchFriend.js
@@ -1,8 +1,21 @@
 Template.searchFriend.events({
   "keyup #searchFriendField": function(event, template){
     event.preventDefault();
-    console.log("mudou!");
-    $("#friendsList").append('<li><a href="/user/messages"><span class="tab">Message Center</span></a></li>');
+    $(".friendListItem").remove();//cleans items from list
+    $("#friendsList").empty();//cleans items from list
+    var searchValue = $("#searchFriendField").val();
+    if(searchValue.length === 0){
+      $("#searchFriendField").val("");
+      $(".friendListItem").remove();
+      $("#friendsList").empty();
+      return;
+    }
+    var regex = new RegExp(".*"+searchValue+".*", "i");
+    var resultQuery = Meteor.users.find({"profile.firstName": {$regex: regex}}).fetch();
+    resultQuery.map((x) => {
+      $("#friendsList").append('<li class="friendListItem"><a href="/user/'+x._id+'"><span class="tab">'+x.profile.firstName+'</span></a></li>');
+    });
+    console.log(resultQuery);
   }
 });
 
